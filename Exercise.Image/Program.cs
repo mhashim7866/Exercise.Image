1228216f61263bb76ed2ea4e316c978e75dd662a
Hashim <Hashim@DESKTOP-TCH42HG>
2023-05-20 16:33:59 +0500

    Add project files.

---
+++ Exercise.Image/Program.cs
@@ -0,0 +1,118 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.SetCursorPosition(45, 0);
+        Console.WriteLine("//,/");
+        
+        Console.SetCursorPosition(82, 0);
+        Console.WriteLine("-~ |");
+
+        Console.SetCursorPosition(43, 1);
+        Console.WriteLine("// / |"); 
+        
+        Console.SetCursorPosition(77, 1);
+        Console.WriteLine("__--~  /  ,");
+
+        Console.SetCursorPosition(41, 2);
+        Console.WriteLine("/'/ / /");
+        
+        Console.SetCursorPosition(75, 2);
+        Console.WriteLine(" __~~ /");
+
+        Console.SetCursorPosition(40, 3);
+        Console.WriteLine("( ( / /'");
+        
+        Console.SetCursorPosition(73, 3);
+        Console.WriteLine("_ -~    _-~ ,/'");
+
+        Console.SetCursorPosition(35, 4);
+        Console.WriteLine(",,)))));,/~--");
+        
+        Console.SetCursorPosition(67, 4);
+        Console.WriteLine("__--~~__-| _-~  _/,");
+
+        Console.SetCursorPosition(35, 4);
+        Console.WriteLine("___(((((((((((;,/~- "); 
+        
+        Console.SetCursorPosition(59, 4);
+        Console.WriteLine(" _--~~--~~  __--~~  --~~  --/,"); 
+
+        Console.SetCursorPosition(30, 5);
+        Console.WriteLine("--/((((((''''(((((((((( >~  / ");
+        
+        Console.SetCursorPosition(57, 5);
+        Console.WriteLine("  /         __--~~ /-~~ _-~ ");
+
+        Console.SetCursorPosition(25, 6);
+        Console.WriteLine("--==//////((''  ,      ')))))), /");
+        
+        Console.SetCursorPosition(56, 6);
+        Console.WriteLine(" /         --~~   __--~'  _-~ ~|");
+
+        Console.SetCursorPosition(23, 7);
+        Console.WriteLine("         ))|  @     ;-.     (((,/");
+        
+        Console.SetCursorPosition(55, 7);
+        Console.WriteLine(" /               __--~~'~~/");
+
+        Console.SetCursorPosition(23, 8);
+        Console.WriteLine("         ( '|      /   )     ))))))/");
+
+        Console.SetCursorPosition(53, 8);
+        Console.WriteLine("))/          ~~~~__/__---~~__--~--_");
+
+        Console.SetCursorPosition(23, 9);
+        Console.WriteLine("            |    _/   |      ");
+
+        Console.SetCursorPosition(51, 9);
+        Console.WriteLine("  (/          ----~~__/__---~~__--~--_--           '");
+
+        Console.SetCursorPosition(23, 10);
+        Console.WriteLine("            o_);      ;       /");
+
+        Console.SetCursorPosition(51, 10);
+        Console.WriteLine("  /          ----~~/           /,-~~/   |         /|");
+
+        Console.SetCursorPosition(23, 11);
+        Console.WriteLine("                    ;        (");
+
+        Console.SetCursorPosition(49, 11);
+        Console.WriteLine("   (          ----~~/           ':::|   |;|        < >");
+
+        Console.SetCursorPosition(49, 12);
+        Console.WriteLine("    '--------~~~~'     /          ':|     /;/------//");
+
+        Console.SetCursorPosition(33, 13);
+        Console.WriteLine("_______/|/   -                        /             /      ~-------~");
+        
+        Console.SetCursorPosition(33, 14);
+        Console.WriteLine("/~;;.____/;;'    /          ____----(          ';;;/");
+        
+        Console.SetCursorPosition(32, 15);
+        Console.WriteLine("/ // _;________;'-------~~~~     |;;/              /");
+        
+        Console.SetCursorPosition(31, 16);
+        Console.WriteLine("// | |                           /  |          /;;,,");
+        
+        Console.SetCursorPosition(30, 17);
+        Console.WriteLine("(<_  | ;                         /',/-------'      _>");
+        
+        Console.SetCursorPosition(30, 18);
+        Console.WriteLine(" (_| ||_                        //~;~~~~~~~~~~~~~~~");
+        
+        Console.SetCursorPosition(30, 19);
+        Console.WriteLine("      '(_|                     (,~~");
+        
+        Console.SetCursorPosition(30, 20);
+        Console.WriteLine("                                ~~/");
+       
+        Console.SetCursorPosition(30, 20);
+        Console.WriteLine("                                ~~-->"); 
+        
+        Console.SetCursorPosition(80, 25);
+        Console.WriteLine("             DESIGN BY HASHIM                   ");
+    }
+
+
+}
